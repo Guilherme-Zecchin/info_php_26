1d5c185ed312daabbb53e0ab1bc35d93e6af55db
Guilherme <zecchin.guilherme1@gmail.com>
2026-04-10 08:07:38 -0300

    Arquivo de estudos

---
+++ Study and Repetition.php
@@ -0,0 +1,23 @@
+<?php
+
+// Mock/Chumbar/fixar = ler dados usuario
+$nome = "Guilherme";
+
+/** Ler os dados de usuario, sendo eles:
+ * nome
+ * idade
+ * sexo
+ * nome pai
+ * nome mae
+ * E escrever a frase: O Usuario de NOME, IDADE, SEXO é filho(a) de:
+ * Nome pai e nome mae
+*/
+$nome = "Guilherme";
+$idade = "29";
+$sexo = "Masculino";
+$Esposa = "Mayara";
+$Endereço = "Rua Marcos Pedro Flaiban, 70";
+$nomeMae = "Sandra"; // camel case
+$nomePai = "Fabio";
+
+echo "Olá $nome, de idade $idade anos, casado com $Esposa e residente da $Endereço <br>";
