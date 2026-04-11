f077ff85ddd99a24a5651990d08e5d1540627481
Guilherme <zecchin.guilherme1@gmail.com>
2026-04-10 22:50:41 -0300

    testing

--- Study and Repetition.php
+++ Study and Repetition.php
@@ -21,3 +21,23 @@ $nomeMae = "Sandra"; // camel case
 $nomePai = "Fabio";
 
 echo "Olá $nome, de idade $idade anos, casado com $Esposa e residente da $Endereço <br>";
+
+/**
+ * Aluno aprovado ou reprovado
+ */
+
+$nota1 = 7;
+$nota2 = 5.9;
+$nota3 = 4.7;
+
+$media = ($nota1 + $nota2 + $nota3) / 3;
+
+$mediaArredondada = round($media, 2);
+
+if ($mediaArredondada >= 7){;
+    echo "O aluno foi aprovado com a média $mediaArredondada <br>";
+}  else {
+    echo "O aluno foi reprovado com a média $mediaArredondada <br>";
+}
+
+echo "<br>";
