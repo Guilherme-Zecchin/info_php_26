6d0e60ca7d648a9a71c869b1daf08ea5fbdc2da1
Guilherme <guizecchin97@gmail.com>
2026-04-13 20:32:28 -0300

    Ex1

--- Exercicios de Fixação.php
+++ Exercicios de Fixação.php
@@ -3,3 +3,14 @@ echo "exercicios de fixação <br>";
 
 //Crie um algoritmo que receba um numero digitado pelo usuario e verifique se esse valor é positivo, negativo ou igual a zero. A saída deve ser: "Valor Positivo, "Valor Negativo" ou "Igual a zero".
 
+$valor = -48; // Valor de entrada
+$resultado = ""; 
+
+if ($valor > 0) {
+    $resultado = "Valor Positivo";
+}elseif ($valor < 0) {
+    $resultado = "Valor Negativo";
+}else {
+    $resultado = "Igual a Zero";
+}
+    echo $resultado ;
