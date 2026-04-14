2b9a085a4055933039e3bfa44f6fea0a053b08c6
Guilherme <guizecchin97@gmail.com>
2026-04-14 19:01:31 -0300

    Ex3

--- Exercicios de Fixação.php
+++ Exercicios de Fixação.php
@@ -17,6 +17,8 @@ if ($valor > 0) {
 
  echo "<br>";
 
+ //OBSERVAÇÕES EXERCICIO 1:
+
 //2. Crie um algoritmo que solicite a entrada e um numero e exiba a tabuada de 0 a 10 de acordo com o número solicitado
 echo "<br>";
 
@@ -25,7 +27,25 @@ $numero = 7;
 for($contador = 0; $contador <= 10; $contador++){
     echo " $numero x $contador = " . $numero * $contador . "<br>";
 }
+//OBSERVAÇÕES EXERCICIO 2:
 
 //3. Crie um algoritmo que solicite um número ee faça o cálculo fatorial do mesmo, exiba o resultado na tela. Ex: Entrada = 3 Processamento: (3 * 2) *1 Saída: 6
 
-echo "testes <br>";
+echo "<br>";
+
+$numero = 12;
+
+if ($numero > 0) {
+    $valor = $numero;
+    for ($i = ($valor - 1); $i > 0; $i--) {
+        $valor = $valor * $i;
+    }
+}
+echo "!{$numero} = {$valor}";
+
+echo "<br>";
+
+//OBSERVAÇÕES EXERCICIO 3: 
+//1) Necessário ter as 3 váriaveis para a variavel $numero não perder seu valor;
+//2) Operação (i = x; $i > 0; $i--) - Significa que, a variavel é x e enquanto ela for menor que 0, continuar diminuindo o valor até chegar a zero;
+//3) Entender qual seria a função da variavel $valor, ver se entra na observação 1;
