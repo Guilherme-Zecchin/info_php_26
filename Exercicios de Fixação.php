563ba114bd37e7c3d751e0857e3337cc168cf08c
suporte <suporte@DESKTOP-LQ0UB60.localdomain>
2026-04-14 18:17:12 -0300

    Ex0.3

--- Exercicios de Fixação.php
+++ Exercicios de Fixação.php
@@ -1,7 +1,7 @@
 <?php
 echo "exercicios de fixação <br>";
 
-//Crie um algoritmo que receba um numero digitado pelo usuario e verifique se esse valor é positivo, negativo ou igual a zero. A saída deve ser: "Valor Positivo, "Valor Negativo" ou "Igual a zero".
+//1. Crie um algoritmo que receba um numero digitado pelo usuario e verifique se esse valor é positivo, negativo ou igual a zero. A saída deve ser: "Valor Positivo, "Valor Negativo" ou "Igual a zero".
 
 $valor = 78; // Valor de entrada
 $resultado = ""; 
@@ -17,10 +17,13 @@ if ($valor > 0) {
 
  echo "<br>";
 
-//Crie um algoritmo que solicite a entrada e um numero e exiba a tabuada de 0 a 10 de acordo com o número solicitado
+//2. Crie um algoritmo que solicite a entrada e um numero e exiba a tabuada de 0 a 10 de acordo com o número solicitado
 
 $numero = 7;
 
 for($contador = 0; $contador <= 10; $contador++){
     echo " $numero x $contador = " . $numero * $contador . "<br>";
 }
+
+//3. Crie um algoritmo que solicite um número ee faça o cálculo fatorial do mesmo, exiba o resultado na tela. Ex: Entrada = 3 Processamento: (3 * 2) *1 Saída: 6
+
