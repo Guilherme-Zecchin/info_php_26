6fda8b659d85cd825b315c15a8d6023b1df98630
Guilherme <guizecchin97@gmail.com>
2026-04-14 18:37:27 -0300

    ajustes2

--- Exercicios de Fixação.php
+++ Exercicios de Fixação.php
@@ -28,5 +28,4 @@ for($contador = 0; $contador <= 10; $contador++){
 
 //3. Crie um algoritmo que solicite um número ee faça o cálculo fatorial do mesmo, exiba o resultado na tela. Ex: Entrada = 3 Processamento: (3 * 2) *1 Saída: 6
 
-echo "<br>";
-
+echo "testes <br>";
