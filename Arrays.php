969747d9f47f5c4529968e66901624d95667fa41
Guilherme <guizecchin97@gmail.com>
2026-05-05 20:47:47 -0300

    Arrays 0.1

--- Arrays.php
+++ Arrays.php
@@ -4,4 +4,19 @@
 
 //Tipos de dados estruturados: Array || vetor, objetos.
 
-//teste git commit
+$numero = 10; // int
+$numero = 10.0; // float || double (decimal)
+
+$listaValores = array(); // array vazio
+$listaValores = []; // array vazio
+
+$listaValores = [10, 20, 3, 50];
+//                0,  1, 2,  3
+
+$tam = count($listaValores); // 4
+
+print_r($listaValores);
+
+for ($i = 0; $i < $tam; $i++) {
+    echo $listaValores[$i] . "<br>";
+}
