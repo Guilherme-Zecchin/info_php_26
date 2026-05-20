984da9c312332e022d0f6523f1b74d60f2a377cd
suporte <suporte@DESKTOP-LQ0UB60.localdomain>
2026-05-19 21:51:19 -0300

    Calculadora Salario

--- Funcionario.php
+++ Funcionario.php
@@ -13,7 +13,7 @@
  * Dica: Utilizar a função number_format() para formatar o salário com duas casas decimais e o símbolo de moeda.
  */
 
-// Executar no terminal:   php ./Funcionario.php || php Funcionario.php
+// Executar no terminal:   php ./funcionario.php || php funcionario.php
 executar();
 
 function executar() {
@@ -21,7 +21,16 @@ function executar() {
         echo "Bem-vindo à Calculadora de Salários!\n";
         echo "-----------------------------\n";
 
+        $salario = lerSalario();  
         exibirMenu();
+        $opcao = lerOpcaoMenu();
+        $opcaoEscolhida = executarOpcao($opcao, $salario);
+
+        if ($opcaoEscolhida == 0) {
+            echo "**Operação inválida!**\n";
+            echo "\n";
+            continue;
+        }
 
         // logica aqui
 
@@ -49,17 +58,65 @@ function desejaContinuar() {
 }
 
 function exibirMenu() {
-    echo "Escolha a opção:\n";
+    echo "\nEscolha a opção:\n";
     echo "1. Calcular Salário\n";
     echo "2. Exibir Aumento\n";
     echo "3. Exibir Descontos\n";
-    echo "4. Exibir Salário Anterior e o Atualizado\n";
+    echo "4. Exibir Salário Anterior e o Atualizado(novo)\n";
     echo "5. Exibir Holerite(contra-cheque)\n";
 }
 
+function lerOpcaoMenu() {
+    echo "\nDigite o número da opção desejada: ";
+    $opcao = readline();
+
+    return $opcao;
+}
+
+function executarOpcao($opcao, $salario) {
+    if ($opcao == '1') {
+        calcularSalario($salario);
+        return 1;
+    } elseif ($opcao == '2') {
+        exibirAumento($salario);
+        return 2;
+    } elseif ($opcao == '3') {
+        exibirDescontos($salario);
+        return 3;
+    } elseif ($opcao == '4') {
+        exibirSalarios($salario);
+        return 4;
+    } elseif ($opcao == '5') {
+        exibirHolerite($salario);
+        return 5;
+    } else {
+         return 0;
+    }
+}
+
 function lerSalario() {
-    echo "Digite o salário do funcionário: ";
+    echo "\nDigite o salário do funcionário: ";
     $salario = readline();
 
     return $salario;
+}
+
+function calcularSalario($salario) {
+    // logica aqui
+}
+
+function exibirAumento($salario) {
+    // logica aqui
+}
+
+function exibirDescontos($salario) {
+    // logica aqui
+}
+
+function exibirSalarios($salario) {
+    // logica aqui
+}
+
+function exibirHolerite($salario) {
+    // logica aqui
 }
