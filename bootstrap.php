ca5c3efcad8c3c00be901a9c449803a54cf82591
Guilherme-Zecchin <guizecchin97@gmail.com>
2026-06-23 21:54:44 -0300

    Update bootstrap.php

--- bootstrap.php
+++ bootstrap.php
@@ -5,3 +5,12 @@ require_once "BancoDeDados.php";
 require_once "FuncionarioModel.php;
 
 $banco = new BancoDeDados($conexao);
+$funcionarioModel = new FuncionarioModel($banco);   
+
+echo "<pre>";
+try {
+    $funcionarios = $funcionarioModel->listar();
+    print_r($funcionarios);
+} catch (Exception $e) {
+    echo $e->getMessage();
+}
