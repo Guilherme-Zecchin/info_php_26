1b256c805508f16a458044d79aa73e4de12bcadb
Guilherme-Zecchin <guizecchin97@gmail.com>
2026-06-30 23:11:34 -0300

    Refactor Funcionario class to use FuncionarioModel

--- Funcionario.php
+++ Funcionario.php
@@ -1,6 +1,7 @@
 <?php
 
 class Funcionario {
+    private FuncionarioModel $funcionarioModel;
     private $nome;
     private $sobrenome;
     private $salario;
@@ -9,166 +10,119 @@ class Funcionario {
     private $cracha;
     private $idPessoa;
 
-    public function __construct($nome, $sobrenome, $salario, $cargo, $setor, $cracha) {
-        $this->nome = $nome;
-        $this->sobrenome = $sobrenome;
-        $this->salario = $salario;
-        $this->cargo = $cargo;
-        $this->setor = $setor;
-        $this->cracha = $cracha;
+    public function __construct(FuncionarioModel $funcionarioModel) {
+        $this->funcionarioModel = $funcionarioModel;
+    }
+
+    private function construirFuncionario($dados) {
+        $this->nome = $dados->nome ?? $this->getNome();
+        $this->sobrenome = $dados->sobrenome ?? $this->getSobrenome();
+        $this->salario = $dados->salario ?? $this->getSalario();
+        $this->cargo = $dados->cargo ?? $this->getCargo();
+        $this->setor = $dados->setor ?? $this->getSetor();
+        $this->cracha = $dados->cracha ?? $this->getCracha();
+        $this->idPessoa = $dados->idPessoa ?? $this->getIdPessoa();
+    }
+
+    private function toArray() {
+        return [
+            'nome' => $this->nome ?? $this->getNome(),
+            'sobrenome' => $this->sobrenome ?? $this->getSobrenome(),
+            'salario' => $this->salario ?? $this->getSalario(),
+            'cargo' => $this->cargo ?? $this->getCargo(),
+            'setor' => $this->setor ?? $this->getSetor(),
+            'cracha' => $this->cracha ?? $this->getCracha(),
+            'idPessoa' => $this->idPessoa ?? $this->getIdPessoa(),
+        ];
+    }
+
+    public function __toString()
+    {
+        return implode(", ", $this->toArray());
+    }
+
+    public function criar($dados) {
+        $this->construirFuncionario($dados);
+        return $this->funcionarioModel->criar($this->toArray());
+    }
+
+    public function atualizar($id, $dados) {
+        $this->construirFuncionario($dados);
+        return $this->funcionarioModel->atualizar($id, $this->toArray());
+    }
+
+    public function listar($filtros = []) {
+        return $this->funcionarioModel->listar($filtros);
+    }
+
+    public function listarPorId($id) {
+        $funcionario = $this->funcionarioModel->listarPorId($id);
+        
+        $this->construirFuncionario($funcionario[0] ?? []);
+        
+        return $this;
+    }
+
+    public function excluir($id) {
+        return $this->funcionarioModel->excluir($id);
+    }
+
+    public function getId() {
+        return $this->id ?? 0;
+    }
+
+    public function getIdPessoa() {
+        return $this->idPessoa ?? 0;
     }
 
     public function getNome() {
         return $this->nome;
     }
 
+    public function setNome($nome) {
+        $this->nome = $nome;
+    }
+
     public function getSobrenome() {
         return $this->sobrenome;
+    }
+
+    public function setSobrenome($sobrenome) {
+        $this->sobrenome = $sobrenome;
     }
 
     public function getSalario() {
         return $this->salario;
     }
 
+    public function setSalario($salario) {
+        $this->salario = $salario;
+    }
+
     public function getCargo() {
         return $this->cargo;
+    }
+
+    public function setCargo($cargo) {
+        $this->cargo = $cargo;
     }
 
     public function getSetor() {
         return $this->setor;
     }
 
+    public function setSetor($setor) {
+        $this->setor = $setor;
+    }
+
     public function getCracha() {
         return $this->cracha;
     }
-}
 
-/**
- * Exercício: Criar um sistema que calcule o aumento de salário de um funcionário e os desconto do INSS e do IRPF,
- * considerando as seguintes regras:
- * - O aumento de salário é de 10% para salários até R$ 1.999.
- * - O aumento de salário é de 3% para salários acima de R$ 2.000.
- * - O desconto do INSS é de 11% para salários acima de R$ 3.000.
- * - O desconto do IRPF é de 0% para salários até R$ 4.500.
- * - O desconto do IRPF é de 22.5% para salários acima de R$ 4.500.
- * Utilizar como exemplo o sistema da calculadora no terminal,
- * onde o usuário irá digitar o salário do funcionário e o sistema irá exibir o salário atualizado com os descontos aplicados.
- * Dica: Utilizar a função number_format() para formatar o salário com duas casas decimais e o símbolo de moeda.
- */
-
-// Executar no terminal:   php ./funcionario.php || php funcionario.php
-executar();
-
-function executar() {
-    do {
-        echo "Bem-vindo à Calculadora de Salários!\n";
-        echo "-----------------------------\n";
-
-        $salario = lerSalario();  
-        exibirMenu();
-        $opcao = lerOpcaoMenu();
-        $opcaoEscolhida = executarOpcao($opcao, $salario);
-
-        if ($opcaoEscolhida == 0) {
-            echo "**Operação inválida!**\n";
-            echo "\n";
-            continue;
-        }
-
-        $resposta = desejaContinuar();
-
-        if ($resposta == 'n' || $resposta == 'N') {
-            break;
-        } 
-
-        echo "\n";
-
-    } while (true);
-}
-
-function desejaContinuar() {
-    echo "\nDeseja realizar outra operação? (s/n): ";
-    $resposta = readline();
-
-    if ($resposta != 'n' && $resposta != 's' && $resposta != 'N' && $resposta != 'S') {
-        echo "Resposta inválida! Por favor, digite 's' para sim ou 'n' para não.\n";
-        return desejaContinuar();
-    } 
-    
-    return $resposta;
-}
-
-function exibirMenu() {
-    echo "\nEscolha a opção:\n";
-    echo "1. Calcular Salário\n";
-    echo "2. Exibir Aumento\n";
-    echo "3. Exibir Descontos\n";
-    echo "4. Exibir Salário Anterior e o Atualizado(novo)\n";
-    echo "5. Exibir Holerite(contra-cheque)\n";
-}
-
-function lerOpcaoMenu() {
-    echo "\nDigite o número da opção desejada: ";
-    $opcao = readline();
-
-    return $opcao;
-}
-
-function executarOpcao($opcao, $salario) {
-    if ($opcao == '1') {
-        calcularSalario($salario);
-        return 1;
-    } elseif ($opcao == '2') {
-        exibirAumento($salario);
-        return 2;
-    } elseif ($opcao == '3') {
-        exibirDescontos($salario);
-        return 3;
-    } elseif ($opcao == '4') {
-        exibirSalarios($salario);
-        return 4;
-    } elseif ($opcao == '5') {
-        exibirHolerite($salario);
-        return 5;
-    } else {
-         return 0;
+    public function setCracha($cracha) {
+        $this->cracha = $cracha;
     }
 }
-
-function lerSalario() {
-    echo "\nDigite o salário do funcionário: ";
-    $salario = readline();
-
-    return $salario;
-}
-
-function formatarSalario($salario) {
-    return number_format($salario, 2, ',', '.');
-}
-
-function calcularAumento($salario) {
-    if ($salario <= 1999) {
-        return $salario * 0.10;
-    } else {
-        return $salario * 0.03;
-    }
-}
-
-function calcularSalario($salario) {
-    $aumento = calcularAumento($salario);
-    $salarioAtualizado = $salario + $aumento;
-    
-    echo "\nSalário atualizado com aumento: R$ " . formatarSalario($salarioAtualizado) . "\n";
-}
-
-function exibirAumento($salario) {
-    // logica aqui
-}
-
-function exibirDescontos($salario) {
-    // logica aqui
-}
-
 function exibirSalarios($salario) {
     // logica aqui
 }
