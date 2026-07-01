734ce39d804b97c3084d629a78ae93508e9cf6e5
Guilherme-Zecchin <guizecchin97@gmail.com>
2026-06-30 23:13:53 -0300

    Clean up bootstrap.php by removing unused code
    Removed commented-out code for listing employees.

--- bootstrap.php
+++ bootstrap.php
@@ -1,16 +1,10 @@
 <?php
 
-require_once "conexao.php";  // GB - Garbagge collector;
+require_once "conexao.php";
 require_once "BancoDeDados.php";
-require_once "FuncionarioModel.php;
+require_once "FuncionarioModel.php";
+require_once "Funcionario.php";
 
 $banco = new BancoDeDados($conexao);
 $funcionarioModel = new FuncionarioModel($banco);   
-
-echo "<pre>";
-try {
-    $funcionarios = $funcionarioModel->listar();
-    print_r($funcionarios);
-} catch (Exception $e) {
-    echo $e->getMessage();
-}
+$funcionario = new Funcionario($funcionarioModel);
