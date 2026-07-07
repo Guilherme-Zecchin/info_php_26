1cd98710d47fd6d60307f9440283194486d32b34
Guilherme-Zecchin <guizecchin97@gmail.com>
2026-07-07 19:28:09 -0300

    Update bootstrap.php

--- bootstrap.php
+++ bootstrap.php
@@ -1,10 +1,14 @@
 <?php
 
-require_once "conexao.php";
-require_once "BancoDeDados.php";
-require_once "FuncionarioModel.php";
-require_once "Funcionario.php";
+session_start();
+
+require_once __DIR__ . "/conexao.php";
+require_once __DIR__ . "/BancoDeDados.php";
+require_once __DIR__ . "/BaseModel.php";
+require_once __DIR__ . "/FuncionarioModel.php";
+require_once __DIR__ . "/Funcionario.php";
+require_once __DIR__ . "/FuncionarioController.php";
 
 $banco = new BancoDeDados($conexao);
-$funcionarioModel = new FuncionarioModel($banco);   
-$funcionario = new Funcionario($funcionarioModel);
+$funcionarioModel = new FuncionarioModel($banco);
+$funcionarioController = new FuncionarioController($funcionarioModel);
